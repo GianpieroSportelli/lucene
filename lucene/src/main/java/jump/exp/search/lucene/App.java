0b9b69f635174fa2c24dd856880ae2803f48fca4
Gianpiero Sportelli <gi.sportelli@reply.it>
2018-05-02 15:19:42 +0200

    first commit

---
+++ lucene/src/main/java/jump/exp/search/lucene/App.java
@@ -0,0 +1,13 @@
+package jump.exp.search.lucene;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
